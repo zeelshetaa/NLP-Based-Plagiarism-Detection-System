65edb9a4df3b82704451abc39f1f0f82a916a98c
zeelshetaa <zeelsheta1958@gmail.com>
2026-04-13 16:59:34 +0530

    Delete NLP_Plagiarism_Detection/preprocessing directory

--- NLP_Plagiarism_Detection/preprocessing/preprocess.py
+++
@@ -1,9 +0,0 @@
-def clean_text(text):
-    if not isinstance(text, str):
-        return ""
-
-    text = text.lower()
-    text = re.sub(r"[^a-z\s]", "", text)
-    words = text.split()
-    words = [w for w in words if w not in stop_words]
-    return " ".join(words)
